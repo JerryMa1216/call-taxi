ef55a23d10825469ab7175c4207daee1ff59bb08
JerryMa1216 <jerryma88@gmail.com>
2014-04-24 11:52:45 +0800

    test commit

--- src/test/java/com/greenisland/taxi/test/PushTest.java
+++ src/test/java/com/greenisland/taxi/test/PushTest.java
@@ -14,8 +14,11 @@ import com.greenisland.taxi.push.request.PushMessageRequest;
 import com.greenisland.taxi.push.response.PushResponse;
 
 public class PushTest {
-	private final static String API_KEY = "Hh3TibjsDoMZWi2NxXGRVGF6";
-	private final static String SECRIT_KEY = "moeDFfZMpbSNoRaH1hDpNfzSLHWf1BM9";
+	private final static String API_KEY = "mMoNDzIMMDEQklAQaj0U30Wo";
+	private final static String SECRIT_KEY = "cgD8ZndUtcecOoT1SCcdIYkE84uk0AVh";
+	
+//	private final static String API_KEY = "Hh3TibjsDoMZWi2NxXGRVGF6";
+//	private final static String SECRIT_KEY = "moeDFfZMpbSNoRaH1hDpNfzSLHWf1BM9";
 
 	// @Test
 	// public void pushNotify() {
@@ -39,33 +42,34 @@ public class PushTest {
 	// System.out.println(response);
 	// }
 
-	// @Test
-	// public void pushMessageAndroid() {
-	// DefaultPushClient client = new DefaultPushClient(new
-	// PushCredentials(API_KEY, SECRIT_KEY));
-	// PushMessageRequest request = new PushMessageRequest();
-	// request.setMessageType(MessageType.message);
-	// StringBuilder message = new StringBuilder("{\"custom\":{");
-	// message.append("\"id\":\"4028688142e7e29c0142e7eda2ab000b\",");
-	// message.append("\"plate\":\"浙A123\",");
-	// message.append("\"name\":\"顾师傅\",");
-	// message.append("\"tel\":\"1231231231\",");
-	// message.append("\"c\":\"159公司\",");
-	// message.append("\"nice\":\"10\",");
-	// message.append("\"order\":\"10\",");
-	// message.append("\"cType\":\"1\",");
-	// message.append("\"lo\":\"121.634877\",");
-	// message.append("\"la\":\"31.219655\"}");
-	// message.append("}");
-	// request.setChannelId("4478218280303304392");
-	// request.setUserId("745409507009129179");
-	// request.setMessages(message.toString());
-	// request.setPushType(PushType.single_user);
-	// request.setDeviceTypes(Arrays.asList(DeviceType.android));
-	// request.setDeployStatus(Long.valueOf(2));
-	// request.setMessageKeys(UUID.randomUUID().toString());
-	// PushResponse<Integer> response = client.pushMessage(request);
-	// System.out.println(response);
-	// }
+	@Test
+	public void pushMessageAndroid() {
+		DefaultPushClient client = new DefaultPushClient(new PushCredentials(
+				API_KEY, SECRIT_KEY));
+		PushMessageRequest request = new PushMessageRequest();
+		request.setMessageType(MessageType.message);
+//		StringBuilder message = new StringBuilder("{\"custom\":{");
+//		message.append("\"id\":\"4028688142e7e29c0142e7eda2ab000b\",");
+//		message.append("\"plate\":\"浙A123\",");
+//		message.append("\"name\":\"顾师傅\",");
+//		message.append("\"tel\":\"1231231231\",");
+//		message.append("\"c\":\"159公司\",");
+//		message.append("\"nice\":\"10\",");
+//		message.append("\"order\":\"10\",");
+//		message.append("\"cType\":\"1\",");
+//		message.append("\"lo\":\"121.634877\",");
+//		message.append("\"la\":\"31.219655\"}");
+//		message.append("}");
+//		request.setChannelId("4478218280303304392");
+//		request.setUserId("745409507009129179");
+		StringBuilder message = new StringBuilder("aaaa");
+		request.setMessages(message.toString());
+		request.setPushType(PushType.all_user);
+		request.setDeviceTypes(Arrays.asList(DeviceType.android));
+		request.setDeployStatus(Long.valueOf(2));
+		request.setMessageKeys(UUID.randomUUID().toString());
+		PushResponse<Integer> response = client.pushMessage(request);
+		System.out.println(response);
+	}
 
 }
